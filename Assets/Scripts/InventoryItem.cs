9ad6454981a221d88f7303ce2dcbfff698fb6034
spoppitty <sarahliu0127@gmail.com>
2025-08-15 01:31:03 -0700

    BaseBuilding pt1
    Added new assets and the Base Building pt1 video's associated files.

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -3,30 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
- 
+
 public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     // --- Is this item trashable --- //
     public bool isTrashable;
- 
+
     // --- Item Info UI --- //
     private GameObject itemInfoUI;
- 
+
     private Text itemInfoUI_itemName;
     private Text itemInfoUI_itemDescription;
     private Text itemInfoUI_itemFunctionality;
- 
+
     public string thisName, thisDescription, thisFunctionality;
- 
+
     // --- Consumption --- //
     private GameObject itemPendingConsumption;
     public bool isConsumable;
- 
+
     public float healthEffect;
     public float caloriesEffect;
     public float hydrationEffect;
- 
- 
+
+    public bool isUseable;
+    public GameObject itemPendingToBeUsed;
+
+
     private void Start()
     {
         itemInfoUI = InventorySystem.Instance.ItemInfoUI;
@@ -34,7 +37,7 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         itemInfoUI_itemDescription = itemInfoUI.transform.Find("itemDescription").GetComponent<Text>();
         itemInfoUI_itemFunctionality = itemInfoUI.transform.Find("itemFunctionality").GetComponent<Text>();
     }
- 
+
     // Triggered when the mouse enters into the area of the item that has this script.
     public void OnPointerEnter(PointerEventData eventData)
     {
@@ -43,13 +46,13 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         itemInfoUI_itemDescription.text = thisDescription;
         itemInfoUI_itemFunctionality.text = thisFunctionality;
     }
- 
+
     // Triggered when the mouse exits the area of the item that has this script.
     public void OnPointerExit(PointerEventData eventData)
     {
         itemInfoUI.SetActive(false);
     }
- 
+
     // Triggered when the mouse is clicked over the item that has this script.
     public void OnPointerDown(PointerEventData eventData)
     {
@@ -62,9 +65,15 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
                 itemPendingConsumption = gameObject;
                 consumingFunction(healthEffect, caloriesEffect, hydrationEffect);
             }
+
+            if (isUseable)
+            {
+                itemPendingToBeUsed = gameObject;
+                UseItem();
+            }
         }
     }
- 
+
     // Triggered when the mouse button is released over the item that has this script.
     public void OnPointerUp(PointerEventData eventData)
     {
@@ -76,29 +85,36 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
                 InventorySystem.Instance.ReCalculateList();
                 CraftingSystem.Instance.RefreshNeededItems();
             }
+
+            if (isUseable && itemPendingToBeUsed == gameObject)
+            {
+                DestroyImmediate(gameObject);
+                InventorySystem.Instance.ReCalculateList();
+                CraftingSystem.Instance.RefreshNeededItems();
+            }
         }
     }
- 
+
     private void consumingFunction(float healthEffect, float caloriesEffect, float hydrationEffect)
     {
         itemInfoUI.SetActive(false);
- 
+
         healthEffectCalculation(healthEffect);
- 
+
         caloriesEffectCalculation(caloriesEffect);
- 
+
         hydrationEffectCalculation(hydrationEffect);
- 
+
     }
- 
- 
+
+
     private static void healthEffectCalculation(float healthEffect)
     {
         // --- Health --- //
- 
+
         float healthBeforeConsumption = PlayerState.Instance.currentHealth;
         float maxHealth = PlayerState.Instance.maxHealth;
- 
+
         if (healthEffect != 0)
         {
             if ((healthBeforeConsumption + healthEffect) > maxHealth)
@@ -111,15 +127,15 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             }
         }
     }
- 
- 
+
+
     private static void caloriesEffectCalculation(float caloriesEffect)
     {
         // --- Calories --- //
- 
+
         float caloriesBeforeConsumption = PlayerState.Instance.currentCalories;
         float maxCalories = PlayerState.Instance.maxCalories;
- 
+
         if (caloriesEffect != 0)
         {
             if ((caloriesBeforeConsumption + caloriesEffect) > maxCalories)
@@ -132,15 +148,15 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             }
         }
     }
- 
- 
+
+
     private static void hydrationEffectCalculation(float hydrationEffect)
     {
         // --- Hydration --- //
- 
+
         float hydrationBeforeConsumption = PlayerState.Instance.currentHydration;
         float maxHydration = PlayerState.Instance.maxHydration;
- 
+
         if (hydrationEffect != 0)
         {
             if ((hydrationBeforeConsumption + hydrationEffect) > maxHydration)
@@ -152,6 +168,58 @@ public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
                 PlayerState.Instance.setHydration(hydrationBeforeConsumption + hydrationEffect);
             }
         }
+    }
+
+    private void UseItem()
+    {
+        itemInfoUI.SetActive(false);
+        InventorySystem.Instance.isOpen = false;
+        InventorySystem.Instance.inventoryScreenUI.SetActive(false);
+
+        CraftingSystem.Instance.isOpen = false;
+        CraftingSystem.Instance.craftingScreenUI.SetActive(false);
+        CraftingSystem.Instance.toolsScreenUI.SetActive(false);
+        CraftingSystem.Instance.survivalScreenUI.SetActive(false);
+        CraftingSystem.Instance.buildScreenUI.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+
+        // probably dont need this
+        //SelectionManager.Instance.EnableSelection();
+        //SelectionManager.Instance.enabled = true;
+
+        switch (gameObject.name)
+        {
+            case "Floor(Clone)":
+                ConstructionManager.Instance.ActivateConstructionPlacement("FloorModel");
+                break;
+
+            case "Floor":
+                ConstructionManager.Instance.ActivateConstructionPlacement("FloorModel");
+                break;
+
+            case "Wall(Clone)":
+                ConstructionManager.Instance.ActivateConstructionPlacement("WallModel");
+                break;
+
+            case "Wall":
+                ConstructionManager.Instance.ActivateConstructionPlacement("WallModel");
+                break;
+
+            case "Campfire(Clone)":
+                ConstructionManager.Instance.ActivateConstructionPlacement("CampfireModel");
+                break;
+
+            case "Campfire":
+                ConstructionManager.Instance.ActivateConstructionPlacement("CampfireModel");
+                break;
+
+            default:
+                break;
+
+        }
+
     }
  
  
